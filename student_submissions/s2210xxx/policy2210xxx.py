3adf1d95e979d6e7767bfb5b6df4df29a9ee6f24
Fluoxetines <dat.nguyenshiesuta@hcmut.edu.vn>
2024-12-02 19:09:51 +0700

    create FFD policy

--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,14 +1,50 @@
 from policy import Policy
 
 
-def Policy2210xxx(Policy):
+class Policy2210xxx(Policy):
     def __init__(self):
         # Student code here
         pass
 
+    # First Fit Decreasing    
     def get_action(self, observation, info):
         # Student code here
-        pass
+        list_prods = observation["products"]
+
+        prod_size = [0, 0]
+        stock_idx = -1
+        pos_x, pos_y = 0, 0
+
+        sorted_prods = sorted(list_prods, key=lambda x: x["size"][0] * x["size"][1], reverse=True)
+
+        for prod in sorted_prods:
+            if prod["quantity"] > 0:
+                prod_size = prod["size"]
+
+                for i, stock in enumerate(observation["stocks"]):
+                    stock_w, stock_h = self._get_stock_size_(stock)
+                    prod_w, prod_h = prod_size
+
+                    if stock_w < prod_w or stock_h < prod_h:
+                        continue
+
+                    pos_x, pos_y = None, None
+                    for x in range(stock_w - prod_w + 1):
+                        for y in range(stock_h - prod_h + 1):
+                            if self._can_place_(stock, (x, y), prod_size):
+                                pos_x, pos_y = x, y
+                                break
+                        if pos_x is not None and pos_y is not None:
+                            break
+
+                    if pos_x is not None and pos_y is not None:
+                        stock_idx = i
+                        break
+
+                if pos_x is not None and pos_y is not None:
+                    break
+
+        return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
 
     # Student code here
     # You can add more functions if needed
