03eee8548a0fb6b9869bae7d18b53cba489fb9bc
Fluoxetines <dat.nguyenshiesuta@hcmut.edu.vn>
2024-12-09 22:34:37 +0700

    add step and get info for branch and bound

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from policy import GreedyPolicy, RandomPolicy
 from student_submissions.s2210xxx.policy2210xxx import Policy2210xxx
 
 import random
+import copy
 
 # Create the environment
 env = gym.make(
     "gym_cutting_stock/CuttingStock-v0",
     render_mode="human",  # Comment this line to disable rendering
 )
-NUM_EPISODES = 15
+NUM_EPISODES = 10
 
 if __name__ == "__main__":
     # Reset the environment
@@ -29,41 +30,41 @@ if __name__ == "__main__":
     #         observation, info = env.reset(seed=ep)
     #         ep += 1
 
-    # # # Reset the environment
-    # # observation, info = env.reset(seed=42)
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
 
-    # # # Test RandomPolicy
-    # # rd_policy = RandomPolicy()
-    # # ep = 0
-    # # while ep < NUM_EPISODES:
-    # #     action = rd_policy.get_action(observation, info)
-    # #     observation, reward, terminated, truncated, info = env.step(action)
+    # # Test RandomPolicy
+    # rd_policy = RandomPolicy()
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     action = rd_policy.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
 
-    # #     if terminated or truncated:
-    # #         print(info)
-    # #         observation, info = env.reset(seed=ep)
-    # #         ep += 1
+    #     if terminated or truncated:
+    #         print(info)
+    #         observation, info = env.reset(seed=ep)
+    #         ep += 1
 
     # # Uncomment the following code to test your policy
-    # # # Reset the environment
-    # # observation, info = env.reset(seed=42)
-    # # print(info)
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
+    # print(info)
 
-    # # policy2210xxx = Policy2210xxx()
-    # # for _ in range(200):
-    # #     action = policy2210xxx.get_action(observation, info)
-    # #     observation, reward, terminated, truncated, info = env.step(action)
-    # #     print(info)
+    # policy2210xxx = Policy2210xxx(policy_id=3)
+    # for _ in range(200):
+    #     action = policy2210xxx.get_action(observation, info)
+    #     observation, reward, terminated, truncated, info = env.step(action)
+    #     print(info)
 
-    # #     if terminated or truncated:
-    # #         observation, info = env.reset()
+    #     if terminated or truncated:
+    #         observation, info = env.reset()
 
     # # Reset the environment
     # observation, info = env.reset(seed=42)
 
     # # Test First Fit Decreasing Policy
     # print("Testing First Fit Decreasing Policy")
-    # policy2210 = Policy2210xxx()
+    # policy2210 = Policy2210xxx(policy_id=3)
     # ep = 0
 
     # while ep < NUM_EPISODES:
@@ -75,41 +76,64 @@ if __name__ == "__main__":
     #         observation, info = env.reset(seed=ep)
     #         ep += 1
 
-    ##################### Test all policy in one loop #####################
-    # create seed list for each episode by random
-    seeds = random.sample(range(100), NUM_EPISODES)
-    seeds[0] = 42
-    print(seeds)
+    # ##################### Test all policy in one loop #####################
+    # # create seed list for each episode by random
+    # seeds = random.sample(range(100), NUM_EPISODES)
+    # seeds[0] = 42
+    # print(seeds)
 
-    # Test all policies
-    print("Testing all policies")
-    policies = [GreedyPolicy(), Policy2210xxx()]
-    ep = 0
-    while ep < NUM_EPISODES:
-        print ("\n==================== Episode {} ====================".format(ep))
-        # reset the environment
-        observation, info = env.reset(seed=seeds[ep])
-        # print env information
-        print("size of stocks: ", len(observation["stocks"]))
-        print("size of products: ", len(observation["products"]))
-        print("total quantity of products: ", sum([prod["quantity"] for prod in observation["products"]]))
-        print("total area of products: ", sum([prod["size"][0] * prod["size"][1] * prod["quantity"] for prod in observation["products"]]))
+    # # Test all policies
+    # print("Testing all policies")
+    # policies = [Policy2210xxx(policy_id=4), Policy2210xxx(policy_id=3)]
+    # policies_name = ["FFD 2", "FFD 1"]
+    # ep = 0
+    # while ep < NUM_EPISODES:
+    #     print ("\n==================== Episode {} ====================".format(ep))
+    #     # reset the environment
+    #     observation, info = env.reset(seed=seeds[ep])
+    #     # print env information
+    #     print("size of stocks: ", len(observation["stocks"]))
+    #     print("size of products: ", len(observation["products"]))
+    #     print("total quantity of products: ", sum([prod["quantity"] for prod in observation["products"]]))
+    #     print("total area of products: ", sum([prod["size"][0] * prod["size"][1] * prod["quantity"] for prod in observation["products"]]))
 
-        for i, policy in enumerate(policies):
-            print("\nTesting {} Policy".format(policy.__class__.__name__))
-            if i != 0:
-                observation, info = env.reset(seed=seeds[ep])
-            while True:
-                action = policy.get_action(observation, info)
-                observation, reward, terminated, truncated, info = env.step(action)
+    #     for i, policy in enumerate(policies):
+    #         print("\nTesting {} Policy".format(policies_name[i]))
+    #         if i != 0:
+    #             observation, info = env.reset(seed=seeds[ep])
+    #         while True:
+    #             action = policy.get_action(observation, info)
+    #             print(action)
+    #             observation, reward, terminated, truncated, info = env.step(action)
 
-                if terminated or truncated:
-                    print(info)
-                    break
+    #             if terminated or truncated:
+    #                 print(info)
+    #                 break
 
-        ep += 1
-            
+    #     ep += 1
 
+    # # Reset the environment
+    # observation, info = env.reset(seed=42)
+    # stock_inx = 20
+    # size1 = observation["products"][0]["size"]
+    # size2 = observation["products"][1]["size"]
+    # position = (0, 0)
+
+    # action = {"stock_idx": [stock_inx, stock_inx + 1], "size": [size1, size2], "position": [position, position]}
+    # observation, reward, terminated, truncated, info = env.step(action)
+    # print(info)
+    # while True:
+    #     pass
+
+    bnb = Policy2210xxx(policy_id=2)
+    observation, info = env.reset(seed=42)
+    bnb.get_action(observation, info)
+
+    ffd = Policy2210xxx(policy_id=3)
+    observation, info = env.reset(seed=42)
+    action = ffd.get_action(observation, info)
+    observation, reward, terminated, truncated, info = env.step(action)
+    print(info)
 
 
 env.close()
--- student_submissions/s2210xxx/policy2210xxx.py
+++ student_submissions/s2210xxx/policy2210xxx.py
@@ -1,13 +1,37 @@
 from policy import Policy
+import numpy as np
+import copy
 
-
+# 1: Column Generation, 2: Branch and Bound, 3: First Fit Decreasing
 class Policy2210xxx(Policy):
-    def __init__(self):
-        # Student code here
+    def __init__(self, policy_id=1):
+        assert policy_id in [1, 2, 3, 4], "Policy ID must be 1 or 2 or 3"
+
+        self.policy_id = policy_id 
+        # parameters for branch and bound
+        self.best_action = []
+        self.best_filled = int(1e9)
+        self.root = None
+
+    def get_action(self, observation, info):
+        if self.policy_id == 1:
+            # Column Generation
+            return self.column_generation_action(observation, info)
+        elif self.policy_id == 2:
+            # Branch and Bound
+            return self.branch_and_bound_action(observation, info)
+        elif self.policy_id == 3:
+            # First Fit Decreasing
+            return self.first_fit_decreasing_action(observation, info)
+        elif self.policy_id == 4:
+            return self.first_fit_decreasing_action_2(observation, info)
+        
+    class Node:
         pass
 
+    ############################################################################################################
     # First Fit Decreasing    
-    def get_action(self, observation, info):
+    def first_fit_decreasing_action(self, observation, info):
         # Student code here
         list_prods = observation["products"]
 
@@ -22,8 +46,8 @@ class Policy2210xxx(Policy):
             if prod["quantity"] > 0:
                 prod_size = prod["size"]
 
-                for i in range (len(sorted_stock_incidies)):
-                    stock = observation["stocks"][sorted_stock_incidies[i]]
+                for i in sorted_stock_incidies:
+                    stock = observation["stocks"][i]
                     stock_w, stock_h = self._get_stock_size_(stock)
                     prod_w, prod_h = prod_size
 
@@ -38,7 +62,7 @@ class Policy2210xxx(Policy):
                                 break
 
                         if pos_x is not None and pos_y is not None:
-                            stock_idx = sorted_stock_incidies[i]
+                            stock_idx = i
                             break
 
                     # if stock cannot fit the product, try to rotate the product
@@ -53,16 +77,13 @@ class Policy2210xxx(Policy):
                             if pos_x is not None and pos_y is not None:
                                 break
                         if pos_x is not None and pos_y is not None:
-                            stock_idx = sorted_stock_incidies[i]
+                            stock_idx = i
                             break
 
                 if pos_x is not None and pos_y is not None:
                     break
 
         return {"stock_idx": stock_idx, "size": prod_size, "position": (pos_x, pos_y)}
-
-    # Student code here
-    # You can add more functions if needed
 
     def area(self, stock1):
         stock1_w, stock1_h = self._get_stock_size_(stock1)
@@ -73,3 +94,227 @@ class Policy2210xxx(Policy):
         sorted_stock_incidies = [i for i in range(len(observation["stocks"]))]
         sorted_stock_incidies = sorted(sorted_stock_incidies, key=lambda x: self.area(observation["stocks"][x]), reverse=True)
         return sorted_stock_incidies
+   
+    ############################################################################################################
+    # branch and bound
+    def branch_and_bound_action(self, observation, info):
+        # initialize base value
+        self.best_action = []
+
+        self.root = Node(observation, info, [])
+        node = self.root
+
+        # use greedy policy to get base value
+        terminated = False
+        while (not terminated):
+            action = self.first_fit_decreasing_action(node.observation, node.info)
+            observation, terminated, info = self._step(action, node.observation, node.info)
+
+            
+    
+    def _step(self, action, observation, info):
+        stock_idx = action["stock_idx"]
+        size = action["size"]
+        position = action["position"]
+
+        width, height = size
+        x, y = position
+
+        # Check if the product is in the product list
+        product_idx = None
+        for i, product in enumerate(observation["products"]):
+            if np.array_equal(product["size"], size) or np.array_equal(
+                product["size"], size[::-1]
+            ):
+                if product["quantity"] == 0:
+                    continue
+
+                product_idx = i  # Product index starts from 0
+                break
+
+        if product_idx is not None:
+            # must make sure stock
+            stock = observation["stocks"][stock_idx]
+            # Check if the product fits in the stock
+            stock_width = np.sum(np.any(stock != -2, axis=1))
+            stock_height = np.sum(np.any(stock != -2, axis=0))
+            if (
+                x >= 0
+                and y >= 0
+                and x + width <= stock_width
+                and y + height <= stock_height
+            ):
+                # Check if the position is empty
+                if np.all(stock[x : x + width, y : y + height] == -1):
+                    # self.cutted_stocks[stock_idx] = 1
+                    stock[x : x + width, y : y + height] = product_idx
+                    observation["products"][product_idx]["quantity"] -= 1
+
+        # An episode is done iff the all product quantities are 0
+        terminated = all([product["quantity"] == 0 for product in observation["products"]])
+        # reward = 1 if terminated else 0  # Binary sparse rewards
+
+        info = self._get_info(observation)
+
+        return observation, terminated, info
+
+    def _get_info(self, observation):
+        cutted_stocks = [np.any(stock > 0) for stock in observation["stocks"]]
+
+        filled_ratio = np.mean(cutted_stocks).item()
+        trim_loss = []
+
+        for sid, stock in enumerate(observation["stocks"]):
+            if cutted_stocks[sid] == 0:
+                continue
+            tl = (stock == -1).sum() / (stock != -2).sum()
+            trim_loss.append(tl)
+
+        trim_loss = np.mean(trim_loss).item() if trim_loss else 1
+
+        return {"filled_ratio": filled_ratio, "trim_loss": trim_loss}
+    
+    ############################################################################################################
+    # Column Generation
+    def column_generation_action(self, observation, info):
+        # Student code here
+
+        pass
+
+class Node:
+    def __init__(self, observation, info, actions):
+        self.children = []
+        self.observation = copy.deepcopy(observation)
+        self.info = info
+        self.actions = actions
+        self.terminate = False
+        self.truncate = False
+
+    def generate_child(self):
+        unfilled_products = [p for p in self.observation["products"] if p["quantity"] > 0]
+        
+
+
+# class Policy2210xxx(Policy):
+#     def __init__(self):
+#         self.best_action = None
+#         self.best_bound = np.inf
+#     def get_action(self, observation, info):
+#         products = copy.deepcopy(observation["products"])
+#         stocks = copy.deepcopy(observation["stocks"])
+
+#         if isinstance(stocks, tuple):
+#             stocks = list(stocks)
+
+#         prod_idx, rotated = self._select_product(products)
+#         if prod_idx is None:
+#             return {"stock_idx": -1, "size": [0, 0], "position": (0, 0)}
+        
+#         prod_size = products[prod_idx]["size"]
+#         if rotated:
+#             prod_size = [prod_size[1], prod_size[0]] #xoay
+
+#         products[prod_idx]["quantity"] -= 1
+
+    
+#         self.best_action = {"stock_idx": -1, "size": prod_size, "position": (0, 0)}
+#         self.best_bound = np.inf
+
+   
+#         self._branch_and_bound(products, stocks, prod_idx, prod_size, [])
+
+#         return self.best_action
+
+#     def _branch_and_bound(self, products, stocks, prod_idx, prod_size, actions_taken):
+       
+#         for s_idx, stock in enumerate(stocks):
+#             stock_w, stock_h = self._get_stock_size_(stock)
+#             w, h = prod_size
+
+#             if stock_w < w or stock_h < h:
+#                 continue 
+
+#             for x in range(stock_w - w + 1):
+#                 for y in range(stock_h - h + 1):
+#                     if self._can_place_(stock, (x, y), prod_size):
+                    
+#                         remaining_space = self._calculate_bound_(stock, (x, y), prod_size)
+
+#                         if remaining_space >= self.best_bound:
+#                             continue 
+                     
+#                         self.best_bound = remaining_space
+#                         self.best_action = {
+#                             "stock_idx": s_idx,
+#                             "size": prod_size,
+#                             "position": (x, y)
+#                         }
+
+                     
+#                         if remaining_space == 0:
+#                             return  
+
+                  
+#                         backup_stock = stocks[s_idx].copy()
+#                         self._place_product(stocks[s_idx], (x, y), prod_size)
+#                         actions_taken.append({
+#                             "stock_idx": s_idx,
+#                             "size": prod_size,
+#                             "position": (x, y)
+#                         })
+
+                       
+#                         next_prod_idx, next_rotated = self._select_product(products)
+#                         if next_prod_idx is not None:
+#                             next_prod_size = products[next_prod_idx]["size"]
+#                             if next_rotated:
+#                                 next_prod_size = [next_prod_size[1], next_prod_size[0]]
+#                             products[next_prod_idx]["quantity"] -= 1
+#                             self._branch_and_bound(products, stocks, next_prod_idx, next_prod_size, actions_taken)
+#                             products[next_prod_idx]["quantity"] += 1  # back
+
+                   
+#                         stocks[s_idx] = backup_stock
+#                         actions_taken.pop()
+
+#     def _select_product(self, products):
+
+#         sorted_products = sorted(
+#             [(i, p["size"], p["quantity"]) for i, p in enumerate(products) if p["quantity"] > 0],
+#             key=lambda x: x[1][0] * x[1][1],
+#             reverse=True
+#         )
+#         for i, size, qty in sorted_products:
+#             # Thử cả hai hướng: không xoay và xoay
+#             for rotated in [False, True]:
+#                 if rotated and size[0] == size[1]:
+#                     continue  # Nếu width == height, không cần xoay
+#                 return i, rotated
+#         return None, False
+
+#     def _calculate_bound_(self, stock, position, size):
+    
+#         x, y = position
+#         w, h = size
+#         stock_w, stock_h = self._get_stock_size_(stock)
+#         remaining_space = (stock_w * stock_h) - (w * h)
+#         return remaining_space
+
+#     def _evaluate_solution(self, stocks):
+       
+#         ratios = []
+#         for stock in stocks:
+#             stock_w, stock_h = self._get_stock_size_(stock)
+#             if stock_w == 0 or stock_h == 0:
+#                 ratios.append(0)
+#                 continue
+#             filled = np.sum(stock != -1)
+#             total = stock_w * stock_h
+#             ratios.append(filled / total if total > 0 else 0)
+#         return np.mean(ratios)
+
+#     def _place_product(self, stock, position, size):
+     
+#         x, y = position
+#         w, h = size
+#         stock[x:x+w, y:y+h] = -1 
